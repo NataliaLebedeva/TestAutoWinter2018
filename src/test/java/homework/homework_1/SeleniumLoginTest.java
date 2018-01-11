f31a74e64bdd2b82a34c480c3243d24316502128
Natalia Lebedeva <natalia.bunyak@gmail.com>
2018-01-11 17:20:04 +0300

    ligin has been performed

--- src/test/java/homework/homework_1/SeleniumLoginTest.java
+++ src/test/java/homework/homework_1/SeleniumLoginTest.java
@@ -1,6 +1,8 @@
 package homework.homework_1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -15,7 +17,14 @@ public class SeleniumLoginTest {
         driver.navigate().to("https://jdi-framework.github.io/tests");
         //3. Assert Browser title
         Assert.assertEquals(driver.getTitle(), "Index Page");
+
         //4. Perform login
+        driver.findElement(By.cssSelector(".uui-profile-menu")).click();
+        driver.findElement(By.id("Login")).sendKeys("epam");
+        driver.findElement(By.id("Password")).sendKeys("1234");
+        driver.findElement(By.cssSelector(".uui-button")).click();
+
+
         //5. Assert User name in the left-top side of screen that user is loggined
         //6. Assert Browser title
         Assert.assertEquals(driver.getTitle(), "Index Page");
@@ -23,8 +32,8 @@ public class SeleniumLoginTest {
         //8. Assert that there are 4 texts on the Home Page and check them by getting texts
         //9. Assert that there are the main header and the text below it on the Home Page
         //10. Close Browser
-        driver.close();
-        driver.quit();
+//        driver.close();
+//        driver.quit();
 
     }
 
