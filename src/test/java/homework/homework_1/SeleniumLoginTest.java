c3851f8c12a0af29d597eb6627c1ae7da43928e5
Natalia Lebedeva <natalia.bunyak@gmail.com>
2018-01-11 17:51:20 +0300

    login has been performed. The name is equals to expected

--- src/test/java/homework/homework_1/SeleniumLoginTest.java
+++ src/test/java/homework/homework_1/SeleniumLoginTest.java
@@ -24,16 +24,19 @@ public class SeleniumLoginTest {
         driver.findElement(By.id("Password")).sendKeys("1234");
         driver.findElement(By.cssSelector(".uui-button")).click();
 
-
         //5. Assert User name in the left-top side of screen that user is loggined
+        WebElement profileInfo = driver.findElement(By.cssSelector(".profile-photo"));
+        String name = profileInfo.getText();
+        Assert.assertTrue("Piter Chailovskii".equalsIgnoreCase(name));
+
         //6. Assert Browser title
         Assert.assertEquals(driver.getTitle(), "Index Page");
         //7. Assert that there are 4 images on the Home Page and they are displayed
         //8. Assert that there are 4 texts on the Home Page and check them by getting texts
         //9. Assert that there are the main header and the text below it on the Home Page
         //10. Close Browser
-//        driver.close();
-//        driver.quit();
+        driver.close();
+        driver.quit();
 
     }
 
