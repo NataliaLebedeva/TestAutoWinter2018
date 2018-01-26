e9989f503b69a51468f72c0267492df061a26003
Natalia Lebedeva <natalia.bunyak@gmail.com>
2018-01-27 00:26:01 +0300

    hw2 fixes 2

---
+++ src/main/java/homework/DriverFactory.java
@@ -0,0 +1,29 @@
+package homework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+public class DriverFactory {
+    private static WebDriver instance;
+    private DriverFactory() {}
+
+    private static HashMap<String, Supplier<WebDriver>> DRIVERS = new HashMap<String, Supplier<WebDriver>>() {{
+        put("chrome", () -> {
+            WebDriver driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+            return driver;
+        });
+    }};
+
+    public static WebDriver GetDriver() {
+        if (Objects.isNull(instance))
+            instance = DRIVERS.get("chrome").get();
+        return instance;
+    }
+}
---
+++ src/test/java/homework/homework_2/ex1/BenefitsTextTestDataProvider_vol2.java
@@ -0,0 +1,58 @@
+package homework.homework_2.ex1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static org.testng.Assert.assertEquals;
+
+public class BenefitsTextTestDataProvider_vol2 {
+    private WebDriver driver;
+
+    @BeforeTest
+    public void beforeClass() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://jdi-framework.github.io/tests/index.htm");
+        new WebDriverWait(driver, 10);
+    }
+
+    @AfterTest
+    public void afterClass() {
+        driver.close();
+    }
+
+    @DataProvider(parallel = true)
+    public Object[][] textBelowPics() {
+        return new Object[][]{
+                {"To include good practices and ideas from successful EPAM projec"},
+                {"To be flexible and customizable"},
+                {"To be multiplatform"},
+                {"Already have good base (about 20 internal and some external projects)," +
+                        " wish to get more\u2026"}
+        };
+    }
+
+    @Test(dataProvider = "textBelowPics")
+    public void textBelowPicTest(String content) throws Exception {
+        List<String> benefits = driver
+                .findElements(By.cssSelector(".benefit-txt")).stream()
+                .map(WebElement::getText)
+                .map(s -> s.replaceAll("\n", " ")).collect(Collectors.toList());
+
+        Assert.assertTrue(benefits.contains(content));
+    }
+
+}
