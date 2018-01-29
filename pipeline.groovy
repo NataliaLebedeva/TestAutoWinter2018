1e301c4c3fafcf70e1746d42b8590aebce2a0941
Natalia Lebedeva <natalia.bunyak@gmail.com>
2018-01-30 00:33:51 +0300

    minor refactoring pipline script

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,4 +1,6 @@
 node {
+    def failedJobName = "";
+
     stage('init') {
         git 'https://github.com/NataliaLebedeva/TestAutoWinter2018'
         sh 'mvn clean compile'
@@ -8,6 +10,7 @@ node {
         try {
             sh 'mvn test -P hw1'
         } catch (e) {
+            failedJobName = "test hw1"
             echo "${e}"
         }
     }
@@ -16,6 +19,7 @@ node {
         try {
             sh 'mvn test -P hw3'
         } catch (e) {
+            failedJobName = "test hw3"
             echo "${e}"
         }
     }
@@ -24,6 +28,7 @@ node {
         try {
             sh 'mvn test -P hw4'
         } catch (e) {
+            failedJobName = "test hw4"
             echo "${e}"
         }
     }
@@ -40,5 +45,8 @@ node {
                  reportName: 'HTML Report',
                  reportTitles: ''
                 ])
+
+        if(!failedJobName.isEmpty())
+            error "Build has failed on job: ${failedJobName} !"
     }
 }
