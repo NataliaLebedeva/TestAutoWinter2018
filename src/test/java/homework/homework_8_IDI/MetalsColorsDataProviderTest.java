63b8c1f00fc96a423a251b0548eafbeece5eb152
Nataliia Lebedeva <Nataliia_Lebedeva@epam.com>
2018-02-02 16:30:11 +0300

    HW8, dataProvider method has been changed

--- src/test/java/homework/homework_8_IDI/MetalsColorsDataProviderTest.java
+++ src/test/java/homework/homework_8_IDI/MetalsColorsDataProviderTest.java
@@ -1,5 +1,7 @@
 package homework.homework_8_IDI;
 
+import com.epam.jdi.uitests.web.selenium.elements.base.J;
+import homework.homework_7_JDI.TestInit;
 import homework.homework_7_JDI.entities.MetalsColorsFormData;
 import homework.homework_7_JDI.entities.User;
 import homework.homework_7_JDI.enums.JDI_PAGES;
@@ -14,7 +16,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
 
-public class MetalsColorsDataProviderTest {
+public class MetalsColorsDataProviderTest extends TestInit{
 
     @BeforeMethod
     public void beforeMethod() {
@@ -33,21 +35,28 @@ public class MetalsColorsDataProviderTest {
     @DataProvider(name = "MetalsColorsData")
     public Object[][] getData() throws FileNotFoundException {
         Map<String, MetalsColorsFormData> map = ResourceLoader.loadResource();
-//        map.entrySet().stream().map(getClass()).toArray();
-       return new Object[][]{{map}};
+
+        Object[] keys = map.keySet().toArray();
+        Object[] values = map.values().toArray();
+        Object[][] result = new Object[map.size()][2];
+        for (int i = 0; i < map.size(); i++) {
+            result[i][0] = keys[i];
+            result[i][1] = values[i];
+        }
+        return result;
     }
 
     @Test(dataProvider = "MetalsColorsData")
-    public void test1() throws IOException {
+    public void test1(MetalsColorsFormData data) throws IOException {
 
         // 2. Open Metals & Colors page by Header menu
         JDISite.homePage.header.menu.selectItem(JDI_PAGES.METALS_COLORS);
 
         // 3. Fill form Metals & Colors by data
-        JDISite.metalsColorsPage.metalsColorsForm.submit(MetalsColorsFormData.DEFAULT);
+        JDISite.metalsColorsPage.metalsColorsForm.submit(data);
 
         // 4. Check Result section
-        JDISite.metalsColorsPage.checkResult(MetalsColorsFormData.DEFAULT);
+        JDISite.metalsColorsPage.checkResult(data);
 
         // 1 create utils
         // 1.1 dataProviderData should be a "singletone"
@@ -55,11 +64,5 @@ public class MetalsColorsDataProviderTest {
         // 2 copy hw_7 and modify it for data provider approach
         // 3 enjoy
 
-//        JsonReader jsonReader = new JsonReader(new FileReader(this.getClass().getClassLoader().getResource("ex8_jdi_metalsColorsDataSet.json").getFile()));
-//
-//        Type token = new TypeToken<Map<String, MetalsColorsFormData>>() {{
-//        }}.getType();
-//
-//        Map<String, MetalsColorsFormData> data = new Gson().fromJson(jsonReader, token);
     }
 }
